3c14951d9947913f89199988482c4b0ecce5b177
tmon <546596046@qq.com>
2019-12-24 15:34:10 +0800

    Update save_images.sh

--- save_images.sh
+++ save_images.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+set -x -e
+
+if [[]]; then
+docker login -u $TARGET_REPO -p $TARGET_REPO_PASSWORD
 
 cat images | while read line
 do
 echo save $line to dockerhub $TARGET_REPO
+docker pull $line
+docker tag $line $TARGET_REPO/${line#*/}
+docker push $TARGET_REPO/${line#*/}
 done
 
